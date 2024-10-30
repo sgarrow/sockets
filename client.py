1ed2d56b9721c8f62999fda5320280793490d6a8
sgarrow <stangarrow@yahoo.com>
2024-10-29 20:48:34 -0700

    Code beautification.

--- client.py
+++ client.py
@@ -3,23 +3,24 @@ import time
 #############################################################################
 
 if __name__ == '__main__':
-    '''Each client will connect to the server with a new address.'''
+
+    # Each client will connect to the server with a new address.
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Replace with the server's address if needed.
-    client_socket.connect(('localhost', 5000))  
+    client_socket.connect(('localhost', 5000))
 
     for ii in range(10):
 
         time.sleep(1)
-    
+
         message = 'Client 1 command number {}'.format(ii)
 
         if ii == 3:
             message = 'close'
 
         client_socket.send(message.encode())
-    
+
         response = client_socket.recv(1024)
         print('1 Response from server: {}'.format(response.decode()))
 
--- server.py
+++ server.py
@@ -1,59 +1,59 @@
-import socket     # For creating and managing sockets.          
-import threading  # For handling multiple clients concurrently. 
+import socket     # For creating and managing sockets.
+import threading  # For handling multiple clients concurrently.
 import time
 #############################################################################
 
-def handle_client(client_socket, client_address):
+def handleClient(clientSocket, clientAddress):
     ''' Handles communication with a single client.
         Receives data from the client, processes it,
-        and sends a response back. '''
+        and sends a response back.'''
 
     print('Accepted connection from: {} {}'.\
-        format(client_address[0], client_address[1]))
+        format(clientAddress[0], clientAddress[1]))
 
     while True:
-        data = client_socket.recv(1024)
-        # Below if not needed.  client_socket.recv blocks by default.
+        data = clientSocket.recv(1024)
+        # Below if not needed.  clientSocket.recv blocks by default.
         #if not data: break
-        print('Received from: {} {}'.format(client_address, data.decode()))
+        print('Received from: {} {}'.format(clientAddress, data.decode()))
 
         # Process data and send response back to the client
         response = 'Message received!'
-        client_socket.send(response.encode())
+        clientSocket.send(response.encode())
 
         if data.decode() == 'close':
-            print('Closing: {}'.format(client_address))
+            print('Closing: {}'.format(clientAddress))
             time.sleep(1)
             break
 
-    client_socket.close()
+    clientSocket.close()
 #############################################################################
 
-def start_server():
+def startServer():
     ''' Starts the server and listens for incoming connections.
         Creates a socket object and binds it to a specified host and port.
         Listens for incoming connections. When a client connects, 
         it creates a new thread to handle that client using the 
-        handle_client function. '''
+        handleClient function. '''
 
     host = '0.0.0.0'  # Listen on all available interfaces
     port = 5000
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.bind((host, port))
+    serverSocket.listen(5)
 
     print('Server listening on: {} {}'.format(host, port))
 
     while True:
-        client_socket, client_address = server_socket.accept()
+        clientSocket, clientAddress = serverSocket.accept()
         # Create a new thread to handle the client
-        thread = threading.Thread( target=handle_client, 
-                                   args=( client_socket, 
-                                          client_address)
+        thread = threading.Thread( target=handleClient,
+                                   args=( clientSocket,
+                                          clientAddress)
                                   )
         thread.start()
 #############################################################################
 
 if __name__ == '__main__':
-    start_server()
+    startServer()
